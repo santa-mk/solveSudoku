10c3e7b87957c91ab72cb1dd611108c93a71b73f
santa-mk <maguielaw.elir.107@gmail.com>
2014-08-03 23:07:16 +0900

    implement input data UI

--- MyApplication/app/src/main/java/santa/mk/jp/solvesudoku/Cell.java
+++ MyApplication/app/src/main/java/santa/mk/jp/solvesudoku/Cell.java
@@ -1,5 +1,7 @@
 package santa.mk.jp.solvesudoku;
 
+import android.widget.TextView;
+
 /**
  * Created by santa_mk on 2014/08/03.
  */
@@ -8,16 +10,19 @@ public class Cell {
     private final int row;
     private final int column;
     private boolean[] possibility;
-    private final boolean isInput;
+    private boolean isInput;
+    private TextView textView;
+    private int orgBgColor;
 
-    public Cell(int row, int column, boolean isInput) {
+    public Cell(int row, int column) {
         this.row = row;
         this.column = column;
         possibility = new boolean[Params.SIZE];
         for (int i = 0; i < Params.SIZE; i++) {
             possibility[i] = true;
         }
-        this.isInput = isInput;
+        orgBgColor = calcOrgBgColor();
+        value = -1;
     }
 
     public int getValue() {
@@ -36,15 +41,55 @@ public class Cell {
         return possibility;
     }
 
+    public TextView getTextView() {
+        return textView;
+    }
+
     public boolean isInput() {
         return isInput;
     }
 
     public void setValue(int value) {
         this.value = value;
+        textView.setText(String.valueOf(value));
+    }
+
+    public void setInput(boolean isInput) {
+        this.isInput = isInput;
+    }
+
+    public void setTextView(TextView textView) {
+        this.textView = textView;
     }
 
     public void losePossibility(int num) {
         this.possibility[num] = false;
+    }
+
+    public void setOrgBGColorToView() {
+        textView.setBackgroundColor(orgBgColor);
+    }
+
+    public void clear() {
+        value = -1;
+        textView.setText("");
+    }
+
+    private int calcOrgBgColor() {
+        if (row < 3) {
+            if (column > 2 && column < 6) {
+                return Params.GRAY;
+            }
+        } else if (row < 6) {
+            if (column < 3 || column > 5) {
+                return Params.GRAY;
+            }
+        } else {
+            if (column > 2 && column < 6) {
+                return Params.GRAY;
+            }
+        }
+
+        return Params.WHITE;
     }
 }
--- MyApplication/app/src/main/java/santa/mk/jp/solvesudoku/Params.java
+++ MyApplication/app/src/main/java/santa/mk/jp/solvesudoku/Params.java
@@ -1,8 +1,17 @@
 package santa.mk.jp.solvesudoku;
 
+import android.content.Context;
+
 /**
  * Created by satan_mk on 2014/08/03.
  */
 public class Params {
     public static final int SIZE = 9;
+    public static int WHITE;
+    public static int GRAY;
+
+    public static void initialize(Context context) {
+        WHITE = context.getResources().getColor(R.color.white);
+        GRAY = context.getResources().getColor(R.color.gray);
+    }
 }
